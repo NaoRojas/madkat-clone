5999979ed04fd83d89d397883000d0a93dd34e5f
naoreds <naorojash@gmail.com>
2025-01-18 19:29:32 -0600

    Wrap CategoryItem in Link for category navigation; update styles for links

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom'
 import CategoryItem from '../category-item/category-item.component'
 
 import './directory.styles.scss'
@@ -6,7 +7,9 @@ const Directory = ({ categories }) => {
   return (
     <div className="directory-container">
       {categories.map((category) => (
-        <CategoryItem key={category.name} category={category} />
+        <Link to={`/shop/${category.category}`} key={category.name}>
+          <CategoryItem key={category.name} category={category} />
+        </Link>
       ))}
     </div>
   )
