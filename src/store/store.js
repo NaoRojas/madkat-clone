218e3ab6c786287ff5fe9305b4f8e0561ab664a0
naoreds <naorojash@gmail.com>
2025-02-05 15:42:00 -0600

    Add redux-persist for state persistence; update store configuration and refactor components for improved state management

--- src/store/store.js
+++ src/store/store.js
@@ -1,7 +1,8 @@
 import { applyMiddleware, compose, createStore } from 'redux'
 import logger from 'redux-logger'
 import { rootReducer } from './root-reducer'
-
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
 const loggerMiddleware = store => next => action => {
   // curried function
   if (!action.type) {
@@ -15,7 +16,16 @@ const loggerMiddleware = store => next => action => {
   console.log('next state:', store.getState())
 }
 
+const persistConfig = {
+  key: 'root',
+  storage,
+  blacklist: ['user'],
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
 //root-reducer
 const middlewares = [logger, loggerMiddleware]
 const composeEnhancers = compose(applyMiddleware(...middlewares))
-export const store = createStore(rootReducer, undefined, composeEnhancers)
+export const store = createStore(persistedReducer, undefined, composeEnhancers)
+export const persistor = persistStore(store)
