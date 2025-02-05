17b4774f22e8fc04b9fb83c067869f8a89165597
naoreds <naorojash@gmail.com>
2025-02-05 15:42:55 -0600

    Update PersistGate to include a loading prop for improved loading state handling

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ import { PersistGate } from 'redux-persist/integration/react'
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <PersistGate persistor={persistor}>
+      <PersistGate persistor={persistor} loading={null}>
         <BrowserRouter>
           <App />
         </BrowserRouter>
