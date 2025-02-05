ec20faab0758a63856a23f12d9d86a3a9c4514cb
naoreds <naorojash@gmail.com>
2025-02-05 16:37:07 -0600

    Add loading spinner to categories and refactor category component for improved UX

--- src/routes/category/category.component.jsx
+++ src/routes/category/category.component.jsx
@@ -1,18 +1,13 @@
 import { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import ProductCard from '../../components/product-card/product-card.component'
-import {
-  selectCategories,
-  selectCategoriesIsLoading,
-} from '../../store/categories/categories.selector'
+import { selectCategories } from '../../store/categories/categories.selector'
 import { useSelector } from 'react-redux'
 import './category.styles.scss'
-import Spinner from '../../components/spinner/spinner.component'
 const Category = () => {
   const { category } = useParams()
   const categories = useSelector(selectCategories)
   const [products, setProducts] = useState([])
-  const isLoading = useSelector(selectCategoriesIsLoading)
 
   useEffect(() => {
     setProducts(categories.find((cat) => cat.category === category)?.products)
@@ -20,18 +15,12 @@ const Category = () => {
 
   return (
     <>
-      {isLoading ? (
-        <Spinner />
-      ) : (
-        <>
-          <h2>{category}</h2>
-          <div className="product-list">
-            {products.map((product) => (
-              <ProductCard key={product.id} product={product} />
-            ))}
-          </div>
-        </>
-      )}
+      <h2>{category}</h2>
+      <div className="product-list">
+        {products.map((product) => (
+          <ProductCard key={product.id} product={product} />
+        ))}
+      </div>
     </>
   )
 }
